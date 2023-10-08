c99e0de35a25dd3a07b3a43c43c42707943071cd
benf <benjamin.frazer.1997@gmail.com>
2023-10-08 13:40:36 +0100

    compile with local verson of libxft

--- config.mk
+++ config.mk
@@ -7,8 +7,8 @@ VERSION = 6.4
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+# X11INC =
+# X11LIB =
 
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
@@ -17,23 +17,25 @@ XINERAMAFLAGS = -DXINERAMA
 # freetype
 FREETYPELIBS = -lfontconfig -lXft
 FREETYPEINC = /usr/include/freetype2
+LIBXFREETYPEINC = $(HOME)/repos/dwm/libxft/include
+LIBXFREETYPEPATH = $(HOME)/repos/dwm/libxft/src/.libs
 # OpenBSD (uncomment)
 #FREETYPEINC = ${X11INC}/freetype2
 #MANPREFIX = ${PREFIX}/man
 
 # includes and libs
-INCS = -I${X11INC} -I${FREETYPEINC}
-LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
+INCS = -I${FREETYPEINC} -I${LIBXFREETYPEINC}
+LIBS = -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
-LDFLAGS  = ${LIBS}
+LDFLAGS  = -Xlinker -rpath=$(LIBXFREETYPEPATH) ${LIBS}
 
 # Solaris
 #CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
-#LDFLAGS = ${LIBS}
+# LDFLAGS = ${LIBS}
 
 # compiler and linker
 CC = cc
